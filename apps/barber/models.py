b5c0f96cbea83cb3e94e6bae66b207a7e10fc806
ulysses-ck <ulisesapaza.280@gmail.com>
2025-04-15 23:13:57 -0300

    fix: include also phone in barber model

--- apps/barber/models.py
+++ apps/barber/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Barber(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
