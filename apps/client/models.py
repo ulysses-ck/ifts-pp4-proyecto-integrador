a8638f6a547c432718d486226d51886a7da0dbee
ulysses-ck <ulisesapaza.280@gmail.com>
2025-04-15 22:38:37 -0300

    feat: define client model fields

--- apps/client/models.py
+++ apps/client/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
