a8ba79537c92aff842d00d7a4c4bfd98f812650a
ulysses-ck <ulisesapaza.280@gmail.com>
2025-04-15 22:51:47 -0300

    chore: include migrations for new models

--- apps/barber/apps.py
+++ apps/barber/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BarberConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'barber'
+    name = 'apps.barber'
--- apps/turn/apps.py
+++ apps/turn/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TurnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'turn'
+    name = 'apps.turn'
