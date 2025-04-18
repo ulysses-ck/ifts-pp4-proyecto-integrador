c81d2e8e70e31433928f6f87fab9592542a00b68
herlazarte <agusmixherlazarte@gmail.com>
2025-04-18 20:11:08 -0300

    Agrego Views y template(de prueba) del cliente

--- apps/client/views.py
+++ apps/client/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from apps.client.models import Client
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class ClientHome(View):
+    def get(self, request):
+        return render(request, 'home_cliente.html')
+    
+class CLientCreateView(CreateView):
+    model = Client
+    template_name = "formulario_client.html"
+    fields = ['name', 'email', 'phone']
+    success_url = reverse_lazy('client:Home_client')
