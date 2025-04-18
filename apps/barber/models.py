353e049f067feb77326cf545dd7995f571d6abd0
herlazarte <agusmixherlazarte@gmail.com>
2025-04-18 18:16:36 -0300

    Se visualizara el nombre del barbero com representacion del objeto

--- apps/barber/models.py
+++ apps/barber/models.py
@@ -4,3 +4,6 @@ class Barber(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
