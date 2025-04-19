d975e90abc5f355055f7538c5ff5e3321f8523f2
herlazarte <agusmixherlazarte@gmail.com>
2025-04-19 15:49:40 -0300

    Agrego metodo str para representar al objeto cliente con su Nombre

--- apps/client/models.py
+++ apps/client/models.py
@@ -4,3 +4,6 @@ class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
