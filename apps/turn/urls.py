ad58117279d907bd94d612f658018ab4350ebc0e
herlazarte <agusmixherlazarte@gmail.com>
2025-04-19 15:51:21 -0300

    Agrego views y template para el ABM del turno

---
+++ apps/turn/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+app_name = 'turn'
+urlpatterns = [
+    path('',views.TurnHome.as_view(), name='home_turn'),
+    path('create/',views.TurnCreate.as_view(), name='turn_create'),
+    path('<int:pk>/update/',views.TurnUpdate.as_view(), name='update_turn'),
+    path('<int:pk>/delete/',views.TurnDelete.as_view(), name='delete_turn'),
+    path('<int:pk>/detail/',views.TurnDetail.as_view(), name='detail_turn'),
+]
--- apps/turn/views.py
+++ apps/turn/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Turn
+from .forms import TurnForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class TurnHome(ListView):
+    model = Turn
+    template_name = "home_turn.html"
+    context_object_name= "turns"
+    
+class TurnCreate(CreateView):
+    model = Turn
+    form_class = TurnForm
+    template_name = "formulario_turn.html"
+    success_url = reverse_lazy('turn:home_turn')
+
+class TurnUpdate(UpdateView):
+    model = Turn
+    form_class = TurnForm
+    template_name = "formulario_turn.html"
+    success_url = reverse_lazy('turn:home_turn')
+
+class TurnDelete(DeleteView):
+    model = Turn
+    template_name = "delete_turn.html"
+    success_url = reverse_lazy('turn:home_turn')
+
+class TurnDetail(DetailView):
+    model = Turn
+    template_name = "detail_turn.html"
+    context_object_name= "turn"
+
