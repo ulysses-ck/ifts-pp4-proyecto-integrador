41b16637558abcf322bec476f41a52725c17fe58
herlazarte <agusmixherlazarte@gmail.com>
2025-04-19 15:54:07 -0300

    Agrego formulario TurnForm para pode manipular los campos date y time para que en el front se muestren como inputs de tipo date y time

---
+++ apps/turn/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Turn  
+
+class TurnForm(forms.ModelForm):
+    class Meta:
+        model = Turn
+        fields = ['client', 'barber', 'date', 'time']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
