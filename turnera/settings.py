b8d939cf6b422a34b8cf5fd3d637bed3990cf81b
Ulises <ulisesapaza.280@gmail.com>
2025-04-28 19:52:32 -0300

    fix: add missing email settings

--- turnera/settings.py
+++ turnera/settings.py
@@ -155,6 +155,15 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email Settings
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+
 # Development-only applications
 try:
     import django_extensions  # noqa
