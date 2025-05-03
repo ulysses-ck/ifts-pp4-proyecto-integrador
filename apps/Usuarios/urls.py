a5fea7a1feeb17e05eb5c014fa5089f02a5a78a9
Ulises <ulisesapaza.280@gmail.com>
2025-05-03 19:20:01 -0300

    fix: remove urls config for register view

--- apps/Usuarios/urls.py
+++ apps/Usuarios/urls.py
@@ -6,7 +6,6 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"), 
     path("logout/", views.logout_view, name="logout"), 
 ]
