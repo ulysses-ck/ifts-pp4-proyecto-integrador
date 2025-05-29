5d83d440632997e2e0d2066b4a3a237900a2a689
gabo87654321 <gabyaguirre84@gmail.com>
2025-05-29 15:04:37 -0300

    renderisar la lista de clientes en homeclient

--- apps/client/urls.py
+++ apps/client/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from . import views
 app_name = 'client' #defino el namespace para las urls
 urlpatterns = [
-    path('',views.ClientHome.as_view(), name='Home_client'),
+    path('',views.ClientListView.as_view(), name='list_client'),
     path('create/',views.CLientCreateView.as_view(), name='create_client'),
-    path('list/', views.ClientListView.as_view(), name='list_client'),
     path('update/<int:pk>/', views.ClientUpdateView.as_view(), name='update_client'),
     path('delete/<int:pk>/', views.ClientDeleteView.as_view(), name='delete_client'),
 ]
