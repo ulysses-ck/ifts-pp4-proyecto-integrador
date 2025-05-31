30a435348b0d719e64a6e78c7d1fd89cdf80dd2e
Ulises <ulisesapaza.280@gmail.com>
2025-05-31 15:54:38 -0300

    create class with login url

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class LandingPageView(TemplateView):
     template_name = 'landing.html' 
+
+
+class MyLoginRequiredMixin(LoginRequiredMixin):
+    login_url = "/user/login"
