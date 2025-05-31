085844e1f754693bdc80dabcaab9e10f095b5755
Ulises <ulisesapaza.280@gmail.com>
2025-05-31 13:46:06 -0300

    fix: separate views into two, week and month

--- apps/turn/urls.py
+++ apps/turn/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'turn'
 
 urlpatterns = [
-    path('',views.TurnHome.as_view(), name='home_turn'),
-    path('create/',views.TurnCreate.as_view(), name='create_turn'),
-    path('<int:pk>/update/',views.TurnUpdate.as_view(), name='update_turn'),
-    path('<int:pk>/delete/',views.TurnDelete.as_view(), name='delete_turn'),
-    path('<int:pk>/detail/',views.TurnDetail.as_view(), name='detail_turn'),
-    path('available/', views.TurnAvailableView.as_view(), name='available_turns'),
+    path('', views.TurnHome.as_view(), name='home_turn'),
+    path('create/', views.TurnCreate.as_view(), name='create_turn'),
+    path('<int:pk>/update/', views.TurnUpdate.as_view(), name='update_turn'),
+    path('<int:pk>/delete/', views.TurnDelete.as_view(), name='delete_turn'),
+    path('<int:pk>/detail/', views.TurnDetail.as_view(), name='detail_turn'),
+    path('available/week/', views.TurnAvailableWeekView.as_view(), name='available_week'),
+    path('available/month/', views.TurnAvailableMonthView.as_view(), name='available_month'),
     path('confirm/<int:pk>/', views.ConfirmTurnView.as_view(), name='confirm_turn'),
 ]
